37698e1e6c6168264570c41469ffaa1fbf69eb0c
Kj McCray <kensvne@Kjs-MacBook-Pro.local>
2020-11-04 09:03:11 -0500

    Due

--- Hard script.js
+++ Hard script.js
@@ -1,12 +1,25 @@
-function add() {
+// let x = 3
 
-};
-function subtract() {
+// let y = 7
 
+function add(x, y) {
+    return x + y
 };
-function multiply() {
+function subtract(x, y) {
+    return x - y
+};
+function multiply(x, y) {
+    return x * y
+};
+function divide(x, y) {
+    return x / y
+};
 
-};
-function divide() {
 
-};
+console.log(add(2, 2));
+
+console.log(subtract(7, 3));
+
+console.log(multiply(3, 4));
+
+console.log(divide(12, 3));
